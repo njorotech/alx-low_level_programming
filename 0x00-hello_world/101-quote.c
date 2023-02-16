c690be9d926e672dd33c5f7c12357fd51f2694c1
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-16 21:25:45 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * @str: input parameter
+  *
+  * Return: 1
+  */
+int main()
+{
+	char* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	return (1);
+}
