ce9cf355bcb6eddc0e4e0b49fa77772749ddbf96
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-18 17:12:18 +0000

    two digits combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i != j)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				if (!(i == 8 && j == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
