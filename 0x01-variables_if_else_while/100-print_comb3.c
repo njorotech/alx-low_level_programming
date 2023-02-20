168881ea9c70dcaf70b60fe966caff32168c972d
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-20 09:12:50 +0000

    combinations of 2 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,15 @@ int main(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (i != j)
+			if ((i == j) || (i > j))
+			{
+
+			}
+			else
 			{
 				putchar('0' + i);
 				putchar('0' + j);
-				if (!(i == 8 && j == 9))
+				if (!((i == 8) && (j == 9)))
 				{
 					putchar(',');
 					putchar(' ');
