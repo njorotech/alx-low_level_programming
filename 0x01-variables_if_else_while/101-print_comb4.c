44990695e9ad25539e0b9dee982902ab8fc441c2
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-20 10:47:27 +0000

    combining 3 numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,29 @@
 int main(void)
 {
 	int i, j, k;
-
+	
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 9; j++)
 		{
 			for (k = 0; k < 10; k++)
 			{
-				putchar('0' + i);
-				putchar('0' + j);
-				putchar('0' + k);
-				if (i == 7 && j == 8 && k == 9)
-					continue;
-				putchar(',');
-				putchar(' ');
+				if (((i == j) && (i == k)) || ((i >= j) || (j >= k)))
+				{
+
+				}
+				else
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					if ((i == 7) && (j == 8) && (k == 9))
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
