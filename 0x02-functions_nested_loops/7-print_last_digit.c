66cf69241a76e5cb6ed41c636bc795906f20f62f
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-21 16:53:55 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * print_last_digit - print the last digit
+  * @n: the number to be computed
+  *
+  * Return: the value of the last digit
+  */
+int print_last_digit(int n)
+{
+	int m = n % 10;
+	return (_putchar(m + '0'));
+}
