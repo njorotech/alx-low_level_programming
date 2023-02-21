2aa14741b05e2fc9b480ab7708a9b35ca310c685
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-21 09:53:09 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i = 0;
 
-	while (c[i] != '\n')
+	while (c[i] != '\0')
 	{
 		putchar(c[i]);
 		i++;
