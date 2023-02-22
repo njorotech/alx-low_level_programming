38b71d544ada9a0ac190df849a7592b9968dee83
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-22 15:14:51 +0000

    Battery street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,22 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n + '0');
+			if ((n >= 0) && (n < 10))
+			{
+
+				_putchar(n + '0');
+			}
+			else if ((n > 9) && (n < 100))
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else if ((n > 99) && (n < 1000))
+			{
+				_putchar((n / 100) + '0');
+				_putchar(((n / 10) % 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 			if (n == 98)
 				continue;
 			_putchar(',');
