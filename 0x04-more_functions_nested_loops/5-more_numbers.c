238a4e2a383cd0efcb6e02e9f01b8d5ab6b45597
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-23 10:08:27 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * more_numbers - write numbers 10 times
+  *
+  * Return: nothing
+  */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
