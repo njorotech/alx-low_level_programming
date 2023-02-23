19970d738a7b45b787b193479249bc2169f7d017
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-23 11:57:50 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_line - draws a straight line
+  * @n: number of times _ should be printed
+  *
+  * Return: 0
+  */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
