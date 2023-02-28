89c0101a0962baf8318bbd61d80fcfc82b0e3c6d
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-02-28 08:05:18 +0000

    print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * puts_half - prints half of a string
+  * @str: string
+  *
+  * Return: nothing
+  */
+void puts_half(char *str)
+{
+	int i = 0;
+	int n;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 == 0)
+	{
+		n = i / 2;
+	}
+	else
+	{
+		n = (i - 1) / 2;
+	}
+
+	for (j = n; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
