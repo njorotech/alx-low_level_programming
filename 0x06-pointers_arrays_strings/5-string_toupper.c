135c66efd0263ee6b79c3b492a630db8239a513a
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-03-02 12:16:29 +0000

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * string_toupper - changes lowercase letters to upper
+  * @s: parameter
+  *
+  * Return: upper case
+  */
+char *string_toupper(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a') && (s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
