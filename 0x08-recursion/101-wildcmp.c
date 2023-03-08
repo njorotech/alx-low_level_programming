b0c55e938c2370010a30a17d1ad48f6b115ef16a
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-03-08 20:08:29 +0000

    compares two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * wildcmp - compares two strings
+  * @s1: first string
+  * @s2: second string
+  *
+  * Return: 1 if strings are identical, otherwise 0
+  */
+int wildcmp(char *s1, char *s2)
+{
+
+}
