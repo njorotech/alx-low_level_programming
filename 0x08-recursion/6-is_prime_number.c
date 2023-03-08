9916d679d7799386b659d962cd8d895157e9bc41
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-03-08 20:00:53 +0000

    checks for a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * is_prime_number - checks for a prime number
+  * @n: number to be checked
+  *
+  * Return: 1 if n is a prime number
+  * 0 if otherwise
+  */
+int is_prime_number(int n)
+{
+	
+}
