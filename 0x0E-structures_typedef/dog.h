3489a044c5c4e4cd4d901586d05d9f63f2047b35
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-03-20 20:44:34 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - defines a dog
+  * @name: first member
+  * @age: second member
+  * @owner: third member
+  *
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
