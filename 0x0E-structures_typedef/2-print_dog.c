26c9834a8f13437a7cacc614844d66ccfd238d3f
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-03-21 13:54:33 +0000

    prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+  * print_dog - prints a struct dog
+  * @d: dog structure
+  *
+  * Return: nothing
+  */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+	
+	}
+
+	if (d->name == NULL)
+	{
+		printf("Name: nil\n");
+	} else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	if (d->age == 0)
+	{
+		printf("nil\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+
+	if (d->owner == NULL)
+	{
+		printf("nil\n");
+	}
+	else
+	{
+		printf("Owner: %s\n",d->owner);
+	}
+}
