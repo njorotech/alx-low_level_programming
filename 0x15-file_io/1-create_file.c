d7929c85a4ddb50a09db9bbf36dd207fc543c207
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-04-08 19:44:31 +0000

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT);
+	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 	{
