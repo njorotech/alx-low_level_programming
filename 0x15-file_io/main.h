42d7d717d5b22d922f4e77d0d650f9346e96500f
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-04-10 12:02:11 +0000

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#define BUFFER_SIZE 1024
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
