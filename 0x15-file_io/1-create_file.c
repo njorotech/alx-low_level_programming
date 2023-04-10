cac64dfc157896fe37624649d7514a7a1c79add1
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-04-10 10:25:56 +0000

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,22 +18,26 @@ int create_file(const char *filename, char *text_content)
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (fd == -1)
+	if (text_content != NULL)
 	{
-		return (-1);
-	}
+		if (fd == -1)
+		{
+			return (-1);
+		}
 
-	while (text_content[i])
-	{
-		len++;
-		i++;
+		while (text_content[i])
+		{
+			len++;
+			i++;
+		}
+		written = write(fd, text_content, len);
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-	written = write(fd, text_content, len);
-	if (written == -1)
-	{
-		return (-1);
-	}
-
+	close(fd);
 	return (1);
 
 }
