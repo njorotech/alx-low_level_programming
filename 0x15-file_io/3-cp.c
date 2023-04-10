8985f5ea30a2b09f0ebe0fd37b30f39f22bbff0f
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-04-10 12:11:43 +0000

    A program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 
 	if (fd2 == -1)
