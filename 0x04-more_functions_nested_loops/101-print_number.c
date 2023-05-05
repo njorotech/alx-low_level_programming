36eeee5d78d7240b034fdb625a8e057fb39d7803
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-05-05 15:18:35 +0000

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,34 +8,22 @@
   */
 void print_number(int n)
 {
-	if ((n >= 0) && (n < 10))
-	{
-		_putchar(n + '0');
-	}
-	else if ((n >= 10) && (n < 100))
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 100) && (n < 1000))
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 1000)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n < 0)
-	{
-		int m = n * -1;
+	int divisor = 1;
 
+	if (n < 0)
+	{
 		_putchar('-');
-		_putchar((m / 10) + '0');
-		_putchar((m % 10) + '0');
+		n *= -1;
+	}
+
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor > 0)
+	{
+		_putchar(((n / divisor) % 10) + '0');
+		divisor /= 10;
 	}
 }
