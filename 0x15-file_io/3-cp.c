bc732fab50b91532edc116a9c722268e5b9fe2b8
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-05-08 20:19:04 +0000

    copies the content of a file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  * @argc: count for arguments passed
+  * @argv: An array of arguments passed to the program
+  *
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2;
+	char buff[BUFFER_SIZE];
+	ssize_t readbytes, writtenbytes;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	fd1 = open(argv[1], O_RDONLY);
+
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((readbytes = read(fd1, buff, BUFFER_SIZE)) > 0)
+	{
+		writtenbytes = write(fd2, buff, readbytes);
+		if (writtenbytes != readbytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
