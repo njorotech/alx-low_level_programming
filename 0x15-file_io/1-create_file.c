f3113dcaa1f9617404e6afeb0a21432e5e155c99
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-05-08 18:43:26 +0000

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	length = strlen(text_content);
-	write(fd, text_content, length);
+	if (text_content != NULL)
+	{
+		write(fd, text_content, length);
+	}
 	close(fd);
 
 	return (1);
