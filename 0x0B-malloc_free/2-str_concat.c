7e894f17fa70b8bf52dc8df7f3959473d8f3a929
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-05-18 13:08:24 +0000

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,14 +16,10 @@ char *str_concat(char *s1, char *s2)
 	int j = 0;
 	int i = 0, k = 0;
 	char *ptr;
-	
-	if (s1 == NULL)
+
+	if ((s1 == NULL) || (s2 == NULL))
 	{
-		return (s2);
-	}
-	else if (s2 == NULL)
-	{
-		return (s1);
+		return (NULL);
 	}
 	while (s1[length] != '\0')
 	{
