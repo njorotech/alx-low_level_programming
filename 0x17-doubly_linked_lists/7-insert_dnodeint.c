2cec0281bb0e62779c30f1ca3d9026860a9fcd13
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-06-15 20:05:13 +0000

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,6 +19,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+	}
+
 	while (current != NULL)
 	{
 		if (i == idx)
