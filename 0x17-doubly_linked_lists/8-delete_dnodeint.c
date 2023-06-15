b2bd0522775295077473e8450d0ed2341a34600e
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-06-15 21:02:34 +0000

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,6 +10,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = *head;
+	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 	{
@@ -25,6 +26,28 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
+
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+	{
+		return (-1);
+	}
+
+	if (current->prev != NULL)
+	{
+		current->prev->next = current->next;
+	}
+
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
+	free(current);
 	return (1);
 
 
